47b340f641790fd7cd3489ae11acb88dead51b40
jimnel <janelson@tcd.ie>
2025-03-26 10:50:28 +0000

    add linear

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -5,6 +5,23 @@ import qse
 
 
 @pytest.mark.parametrize("lattice_spacing", [0.5, 3.1])
+@pytest.mark.parametrize("N", [2, 3])
+def test_linearlattice(lattice_spacing, N):
+    qbits = qse.utils.linear(lattice_spacing, N)
+    assert isinstance(qbits, qse.Qbits)
+    assert qbits.nqbits == N
+
+    # Check the distance between the first qubit and it's nearest neighbour
+    # is the lattice spacing.
+    assert np.isclose(qbits.get_all_distances()[0][1:].min(), lattice_spacing)
+
+    _angles = [90] * 3
+    assert np.allclose(
+        qbits.cell.cellpar(), np.array([lattice_spacing * N, 0, 0] + _angles)
+    )
+
+
+@pytest.mark.parametrize("lattice_spacing", [0.5, 3.1])
 @pytest.mark.parametrize("N1", [2, 3])
 @pytest.mark.parametrize("N2", [2, 3])
 def test_squarelattice(lattice_spacing, N1, N2):
@@ -15,3 +32,9 @@ def test_squarelattice(lattice_spacing, N1, N2):
     # Check the distance between the first qubit and it's nearest neighbour
     # is the lattice spacing.
     assert np.isclose(qbits.get_all_distances()[0][1:].min(), lattice_spacing)
+
+    _angles = [90] * 3
+    assert np.allclose(
+        qbits.cell.cellpar(),
+        np.array([lattice_spacing * N1, lattice_spacing * N2, 0] + _angles),
+    )
