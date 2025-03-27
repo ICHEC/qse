2b3ba4d57b4f7050f55f2d603ccbfa842a584312
jimnel <janelson@tcd.ie>
2025-03-27 14:44:28 +0000

    lattice library

---
+++ qse/lattice_library.py
@@ -0,0 +1,175 @@
+"""
+A collection of convenience functions for creating common lattices.
+"""
+
+import numpy as np
+
+import qse
+
+
+def _check_repeats(repeats, param_name):
+    if not isinstance(repeats, int) or repeats < 2:
+        raise Exception(
+            f"The {param_name} must be an integer greater than 1. {param_name}={repeats}"
+        )
+
+
+def _lattice_creator(
+    unit_cell_2d: list,
+    lattice_spacing: float,
+    repeats_x: int,
+    repeats_y: int,
+    qubit_positions: list = [[0, 0, 0]],
+):
+    """Create a Qbits object from a 2d cell, lattice_spacing, repeats and qubit positions."""
+
+    # add repeats check
+    """Helper function to create the lattices."""
+    unit = qse.Qbits(positions=np.array(qubit_positions))
+
+    # We add [[0, 0, 0]] to convert the unit cell into 3d.
+    unit.cell = lattice_spacing * qse.cell.Cell(unit_cell_2d + [[0, 0, 0]])
+    return unit.repeat((repeats_x, repeats_y, 1))
+
+
+def linear_chain(lattice_spacing: float = 1.0, repeats: int = 6):
+    """
+    Generate a Qbits object in linear chain geometry.
+
+    Parameters
+    ----------
+    lattice_spacing : float
+        The lattice spacing.
+    repeats : int
+        The number of repeats. Must be greater than 1.
+
+    Returns
+    -------
+    Qbits
+        The Qbits lattice.
+    """
+    _check_repeats(repeats, "repeats")
+    return _lattice_creator([[1, 0, 0], [0, 0, 0]], lattice_spacing, repeats, 1)
+
+
+def square_lattice(
+    lattice_spacing: float = 1.0, repeats_x: int = 2, repeats_y: int = 2
+):
+    """
+    Generate a Qbits object in square lattice geometry.
+
+    Parameters
+    ----------
+    lattice_spacing : float
+        The lattice spacing.
+    repeats_x : int
+        The number of repeats in the x direction. Must be greater than 1.
+    repeats_y : int
+        The number of repeats in the y direction. Must be greater than 1.
+
+    Returns
+    -------
+    Qbits
+        The Qbits lattice.
+    """
+    _check_repeats(repeats_x, "repeats_x")
+    _check_repeats(repeats_y, "repeats_y")
+    return _lattice_creator(
+        [[1, 0, 0], [0, 1, 0]], lattice_spacing, repeats_x, repeats_y
+    )
+
+
+def triangular_lattice(
+    lattice_spacing: float = 1.0, repeats_x: int = 2, repeats_y: int = 2
+):
+    """
+    Generate a Qbits object in triangular lattice geometry.
+
+    Parameters
+    ----------
+    lattice_spacing : float
+        The lattice spacing.
+    repeats_x : int
+        The number of repeats in the x direction. Must be greater than 1.
+    repeats_y : int
+        The number of repeats in the y direction. Must be greater than 1.
+
+    Returns
+    -------
+    Qbits
+        The Qbits lattice.
+    """
+    _check_repeats(repeats_x, "repeats_x")
+    _check_repeats(repeats_y, "repeats_y")
+    return _lattice_creator(
+        [[1, 0, 0], [0.5, np.sqrt(3) / 2, 0]], lattice_spacing, repeats_x, repeats_y
+    )
+
+
+def hexagonal_lattice(
+    lattice_spacing: float = 1.0, repeats_x: int = 2, repeats_y: int = 2
+):
+    """
+    Generate a Qbits object in hexagonal lattice geometry.
+
+    Parameters
+    ----------
+    lattice_spacing : float
+        The lattice spacing.
+    repeats_x : int
+        The number of repeats in the x direction. Must be greater than 1.
+    repeats_y : int
+        The number of repeats in the y direction. Must be greater than 1.
+
+    Returns
+    -------
+    Qbits
+        The Qbits lattice.
+    """
+    _check_repeats(repeats_x, "repeats_x")
+    _check_repeats(repeats_y, "repeats_y")
+    return _lattice_creator(
+        [
+            [3 / 2, np.sqrt(3) / 2, 0],
+            [3 / 2, -np.sqrt(3) / 2, 0],
+        ],
+        lattice_spacing,
+        repeats_x,
+        repeats_y,
+        [[0, 0, 0], [lattice_spacing, 0, 0]],
+    )
+
+
+def kagome_lattice(
+    lattice_spacing: float = 1.0, repeats_x: int = 2, repeats_y: int = 2
+):
+    """
+    Generate a Qbits object in kagome lattice geometry.
+
+    Parameters
+    ----------
+    lattice_spacing : float
+        The lattice spacing.
+    repeats_x : int
+        The number of repeats in the x direction. Must be greater than 1.
+    repeats_y : int
+        The number of repeats in the y direction. Must be greater than 1.
+
+    Returns
+    -------
+    Qbits
+        The Qbits lattice.
+    """
+    _check_repeats(repeats_x, "repeats_x")
+    _check_repeats(repeats_y, "repeats_y")
+    return _lattice_creator(
+        [[2, 0, 0], [1, np.sqrt(3), 0]],
+        lattice_spacing,
+        repeats_x,
+        repeats_y,
+        [
+            [0, 0, 0],
+            [lattice_spacing, 0, 0],
+            [lattice_spacing / 2, np.sqrt(3) * lattice_spacing / 2, 0],
+        ],
+    )
---
+++ tests/lattice_library_test.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pytest
+
+import qse
+from qse.lattice_library import (
+    hexagonal_lattice,
+    kagome_lattice,
+    linear_chain,
+    square_lattice,
+    triangular_lattice,
+)
+
+_lattice_spacings = [0.5, 3.1]
+_repeats = [2, 3]
+
+
+def _lattice_checker(qbits, expected_qbits, lattice_spacing, expected_cellpar):
+    assert isinstance(qbits, qse.Qbits)
+    assert qbits.nqbits == expected_qbits
+
+    # Check the distance between the first qubit and it's nearest neighbour
+    # is the lattice spacing.
+    assert np.isclose(qbits.get_all_distances()[0][1:].min(), lattice_spacing)
+
+    # Check the cellpar corresponds to what we expect.
+    assert np.allclose(qbits.cell.cellpar(), expected_cellpar)
+
+
+@pytest.mark.parametrize("lattice_spacing", _lattice_spacings)
+@pytest.mark.parametrize("N", _repeats)
+def test_linear(lattice_spacing, N):
+    qbits = linear_chain(lattice_spacing, N)
+    _lattice_checker(qbits, N, lattice_spacing, [lattice_spacing * N, 0, 0, 90, 90, 90])
+
+
+def test_linear_fail():
+    with pytest.raises(
+        Exception, match="The repeats must be an integer greater than 1. repeats=1"
+    ):
+        linear_chain(1.0, 1)
+
+
+@pytest.mark.parametrize("lattice_spacing", _lattice_spacings)
+@pytest.mark.parametrize("N1", _repeats)
+@pytest.mark.parametrize("N2", _repeats)
+@pytest.mark.parametrize(
+    "lattice_func, angles",
+    [
+        (square_lattice, [90, 90, 90]),
+        (triangular_lattice, [90, 90, 60]),
+    ],
+)
+def test_square_and_triangular(lattice_func, lattice_spacing, N1, N2, angles):
+    qbits = lattice_func(lattice_spacing, N1, N2)
+    _lattice_checker(
+        qbits,
+        N1 * N2,
+        lattice_spacing,
+        [lattice_spacing * N1, lattice_spacing * N2, 0] + angles,
+    )
+
+
+@pytest.mark.parametrize("lattice_spacing", _lattice_spacings)
+@pytest.mark.parametrize("N1", _repeats)
+@pytest.mark.parametrize("N2", _repeats)
+def test_hexagonal(lattice_spacing, N1, N2):
+    qbits = hexagonal_lattice(lattice_spacing, N1, N2)
+    _lattice_checker(
+        qbits,
+        N1 * N2 * 2,
+        lattice_spacing,
+        [lattice_spacing * N1 * np.sqrt(3), lattice_spacing * N2 * np.sqrt(3), 0]
+        + [90, 90, 60],
+    )
+
+
+@pytest.mark.parametrize("lattice_spacing", _lattice_spacings)
+@pytest.mark.parametrize("N1", _repeats)
+@pytest.mark.parametrize("N2", _repeats)
+def test_kagome(lattice_spacing, N1, N2):
+    qbits = kagome_lattice(lattice_spacing, N1, N2)
+    _lattice_checker(
+        qbits,
+        N1 * N2 * 3,
+        lattice_spacing,
+        [lattice_spacing * N1 * 2, lattice_spacing * N2 * 2, 0] + [90, 90, 60],
+    )
+
+
+@pytest.mark.parametrize(
+    "lattice_func",
+    [
+        square_lattice,
+        triangular_lattice,
+        hexagonal_lattice,
+        kagome_lattice,
+    ],
+)
+def test_lattices_fail(lattice_func):
+    with pytest.raises(
+        Exception, match="The repeats_x must be an integer greater than 1. repeats_x=1"
+    ):
+        lattice_func(1.0, 1, 2)
+
+    with pytest.raises(
+        Exception, match="The repeats_y must be an integer greater than 1. repeats_y=1"
+    ):
+        lattice_func(1.0, 2, 1)
