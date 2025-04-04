0b88a40a1b7b28e84728a0cb1984095015d142a7
jimnel <janelson@tcd.ie>
2025-04-04 16:09:11 +0100

    remove myqlm server

--- qse_myqlm_spinup.py
+++ qse_myqlm_spinup.py
@@ -1,6 +1,5 @@
 import qse 
 import qat.qpus
-import qlmaas.qpus
 import numpy as np
 
 qbits = qse.Qbits(positions=np.ones((4, 3)))
@@ -9,5 +8,4 @@ qbits = qse.Qbits(positions=np.ones((4, 3)))
 print("qse", qse.__version__)
 print("np", np.__version__)
 print("qat.qpus", qat.qpus.__version__)
-print("qlmaas.qpus", qlmaas.qpus.__version__)
 print("All ok!")
