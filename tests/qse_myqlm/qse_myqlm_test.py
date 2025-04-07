d911025254e4184adf28cbf97bc16f3617e57cf9
jimnel <janelson@tcd.ie>
2025-04-07 14:34:34 +0100

    update test directory

---
+++ tests/qse_myqlm/qse_myqlm_test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import qat.qpus
+
+import qse
+
+def qse_test():
+    qbits = qse.Qbits(positions=np.ones((4, 3)))
---
+++ tests/qse_pulser/qse_pulser_test.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pulser
+
+import qse
+
+def myqlm_test():
+    qbits = qse.Qbits(positions=np.ones((4, 3)))
+    print("pulser", pulser.__version__)
