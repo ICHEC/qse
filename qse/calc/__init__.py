7b5a82b0ee5788c1e56b8bffbce665fe1e4220fb
jimnel <janelson@tcd.ie>
2025-04-07 16:40:35 +0100

    update __init__

--- qse/calc/__init__.py
+++ qse/calc/__init__.py
@@ -1,9 +1,10 @@
 """Interface to different QSE calculators."""
 
-__all__ = ["abc", "calculator", "signal"]
+__all__ = ["abc", "calculator", "Signal", "PropertyNotImplementedError", "PropertyNotPresent", "CalculatorSetupError", "CalculationFailed"]
 
 import qse.calc.calculator
 from qse.calc.signal import Signal
+from qse.calc.messages import PropertyNotImplementedError, PropertyNotPresent, CalculatorSetupError, CalculationFailed
 
 from .myqlm import Myqlm
 from .pulser import Pulser
