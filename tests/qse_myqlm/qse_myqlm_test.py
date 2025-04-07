4b87c2993614aad50fcf43b87c11a1b477cb0a47
jimnel <janelson@tcd.ie>
2025-04-07 14:35:22 +0100

    isort

--- tests/qse_myqlm/qse_myqlm_test.py
+++ tests/qse_myqlm/qse_myqlm_test.py
@@ -3,5 +3,6 @@ import qat.qpus
 
 import qse
 
+
 def qse_test():
     qbits = qse.Qbits(positions=np.ones((4, 3)))
--- tests/qse_pulser/qse_pulser_test.py
+++ tests/qse_pulser/qse_pulser_test.py
@@ -3,6 +3,7 @@ import pulser
 
 import qse
 
+
 def myqlm_test():
     qbits = qse.Qbits(positions=np.ones((4, 3)))
     print("pulser", pulser.__version__)
