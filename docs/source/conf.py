e6d3d571022761e00c4144380145f38931554b7f
jimnel <janelson@tcd.ie>
2025-05-06 13:55:47 +0100

    initial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'qse'
+copyright = '2025, James'
+author = 'James'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    ]
+templates_path = ['_templates']
+exclude_patterns = []
+
+# napoleon settings
+napoleon_numpy_docstring = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+import qse
---
+++ docs/source/index.rst
@@ -0,0 +1,28 @@
+.. qse documentation master file, created by
+   sphinx-quickstart on Tue May  6 13:34:11 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+qse documentation
+=================
+Welcome to qse
+
+.. autoclass:: qse.Qbits
+    :members:
+
+.. autoclass:: qse.Qbit
+    :members:
+
+.. automodule:: qse.utils
+    :members:
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+
+Indices and tables
+==================
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
