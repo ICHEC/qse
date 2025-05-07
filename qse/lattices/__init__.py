8a32a05c735e565da1b8ac6d40475c4197bd42eb
jimnel <janelson@tcd.ie>
2025-05-07 15:23:05 +0100

    black

--- qse/lattices/__init__.py
+++ qse/lattices/__init__.py
@@ -1,5 +1,17 @@
 """A collection of convenience functions for creating common lattices."""
 
-__all__ = ["linear_chain", "square_lattice", "triangular_lattice", "hexagonal_lattice", "kagome_lattice"]
+__all__ = [
+    "linear_chain",
+    "square_lattice",
+    "triangular_lattice",
+    "hexagonal_lattice",
+    "kagome_lattice",
+]
 
-from .lattices import linear_chain, square_lattice, triangular_lattice, hexagonal_lattice, kagome_lattice
+from .lattices import (
+    linear_chain,
+    square_lattice,
+    triangular_lattice,
+    hexagonal_lattice,
+    kagome_lattice,
+)
