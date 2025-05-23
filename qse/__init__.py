fd1110a19c4e11f20de207fc0ac9955c77437ace
rajarshi <rajarshi.tiwari@ichec.ie>
2025-05-23 15:27:00 +0100

    manual isort

--- qse/__init__.py
+++ qse/__init__.py
@@ -10,7 +10,7 @@ __version__ = "0.1.2"
 
 from ase import cell
 
-from qse import calc, utils, magnetic
+from qse import calc, magnetic, utils
 from qse.qbit import Qbit
 from qse.qbits import Qbits
 from qse.signal import Signal
