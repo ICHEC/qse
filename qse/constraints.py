c3aab665c51d3285b7f54dcde2f7864d8e7421e6
Rajarshi Tiwari <rajarshi.tiwari@ichec.ie>
2025-06-27 11:22:45 +0100

    Removed FixScaled

--- qse/constraints.py
+++ qse/constraints.py
@@ -11,7 +11,6 @@ __all__ = [
     "FixedMode",
     "FixConstraintSingle",
     "FixQbits",
-    "FixScaled",
     "FixCom",
     "FixedPlane",
     "FixConstraint",
@@ -486,43 +485,3 @@ class FixCartesian(FixConstraintSingle):
             "name": "FixCartesian",
             "kwargs": {"a": self.a, "mask": ~self.mask.tolist()},
         }
-
-
-class FixScaled(FixConstraintSingle):
-    "Fix an atom index *a* in the directions of the unit vectors."
-
-    def __init__(self, cell, a, mask=(1, 1, 1)):
-        self.cell = np.asarray(cell)
-        self.a = a
-        self.mask = np.array(mask, bool)
-
-    def get_removed_dof(self, atoms):
-        return self.mask.sum()
-
-    def adjust_positions(self, atoms, new):
-        scaled_old = atoms.cell.scaled_positions(atoms.positions)
-        scaled_new = atoms.cell.scaled_positions(new)
-        for n in range(3):
-            if self.mask[n]:
-                scaled_new[self.a, n] = scaled_old[self.a, n]
-        new[self.a] = atoms.cell.cartesian_positions(scaled_new)[self.a]
-
-    def adjust_forces(self, atoms, forces):
-        # Forces are covarient to the coordinate transformation,
-        # use the inverse transformations
-        scaled_forces = atoms.cell.cartesian_positions(forces)
-        scaled_forces[self.a] *= -(self.mask - 1)
-        forces[self.a] = atoms.cell.scaled_positions(scaled_forces)[self.a]
-
-    def todict(self):
-        return {
-            "name": "FixScaled",
-            "kwargs": {
-                "a": self.a,
-                "cell": self.cell.tolist(),
-                "mask": self.mask.tolist(),
-            },
-        }
-
-    def __repr__(self):
-        return "FixScaled(%s, %d, %s)" % (repr(self.cell), self.a, repr(self.mask))
