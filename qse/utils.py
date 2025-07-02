17374dc08b02125a3e02d914a63a5f4c7d5acbda
jimnel <janelson@tcd.ie>
2025-07-02 12:45:43 +0100

    update utils

--- qse/utils.py
+++ qse/utils.py
@@ -7,9 +7,26 @@ import numpy as np
 
 def int2bin(x, width=32):
     """
-    converts an integer array to array of equivalent binary strings.
-    Equivalent to:
-        int2bin = np.vectorize(lambda x, width=16: np.binary_repr(x,width=width))
+    Converts an integer array to array of equivalent binary strings.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        An array of integers.
+    width : int
+        The length of the binary strings.
+    
+    Returns
+    -------
+    np.ndarray
+        The array of binary strings.
+
+    Notes
+    -----
+    This function is equivalent to:
+    
+    >>> int2bin = np.vectorize(lambda x, width=16: np.binary_repr(x,width=width))
+
     However vectorize version is a bit slower compared to the one below.
     """
     out = np.fromiter(
