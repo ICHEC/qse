4a1f10749d781fbe59d3f7bfff2c865fdefe5de1
James Nelson <ja94nelson@gmail.com>
2025-08-25 14:49:26 +0100

    funcs in test

--- tests/qse/magnetic_test.py
+++ tests/qse/magnetic_test.py
@@ -8,23 +8,10 @@ pauli_y = np.array([[0.0, -1.0j], [1.0j, 0.0]])
 pauli_z = np.array([[1.0, 0.0], [0.0, -1.0]])
 
 
-def test_spin_two_qubits():
-    """Check get_spins works for two qubits."""
-    n = 2
-    hdim = 2**n
+def random_state(hdim):
     statevector = np.random.rand(hdim) + 1j * np.random.rand(hdim)
     statevector /= np.linalg.norm(statevector)
-
-    def expval(ops, state):
-        return [(np.conj(state) @ (op @ state[:, None])).item() for op in ops]
-
-    spin_qse = qse.magnetic.get_spins(statevector, n)
-
-    op_q0 = [np.kron(p, np.eye(2)) for p in [pauli_x, pauli_y, pauli_z]]
-    op_q1 = [np.kron(np.eye(2), p) for p in [pauli_x, pauli_y, pauli_z]]
-    spin_np = np.stack([expval(op_q0, statevector), expval(op_q1, statevector)])
-
-    assert np.allclose(spin_qse, spin_np)
+    return statevector
 
 
 def kron_list(ops):
@@ -34,6 +21,26 @@ def kron_list(ops):
     for op in ops[1:]:
         x = np.kron(x, op)
     return x
+
+
+def expval(op, state):
+    return (np.conj(state) @ (op @ state[:, None])).item()
+
+
+def test_spin_two_qubits():
+    """Check get_spins works for two qubits."""
+    n = 2
+    statevector = random_state(2**n)
+    spin_qse = qse.magnetic.get_spins(statevector, n)
+
+    op_q0 = [np.kron(p, np.eye(2)) for p in [pauli_x, pauli_y, pauli_z]]
+    expval_q0 = [expval(op, statevector) for op in op_q0]
+    op_q1 = [np.kron(np.eye(2), p) for p in [pauli_x, pauli_y, pauli_z]]
+    expval_q1 = [expval(op, statevector) for op in op_q1]
+
+    spin_np = np.stack([expval_q0, expval_q1])
+
+    assert np.allclose(spin_qse, spin_np)
 
 
 @pytest.mark.parametrize("basis_state", ["1", "00", "001", "101011"])
@@ -49,14 +56,13 @@ def test_number_operator_basis(basis_state):
 @pytest.mark.parametrize("n", [1, 2, 3])
 def test_number_operator_random(n):
     """Check the number operator for random states."""
-    psi = np.random.rand(2**n) + 1j * np.random.rand(2**n)
-    psi /= np.linalg.norm(psi)
+    psi = random_state(2**n)
 
     np_n_op = np.zeros(n, dtype=complex)
     for i in range(n):
         op_list = [np.eye(2)] * n
         op_list[i] = np.array([[0.0, 0.0], [0.0, 1.0]])
-        np_n_op[i] = (np.conj(psi) @ (kron_list(op_list) @ psi[:, None])).item()
+        np_n_op[i] = expval(kron_list(op_list), psi)
 
     n_op = qse.magnetic.get_number_operator(psi, n)
     assert np.allclose(n_op, np_n_op)
