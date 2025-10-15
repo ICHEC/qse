3830f9f9da8ee96b88fc354569819182f82a16a0
James Nelson <ja94nelson@gmail.com>
2025-10-15 15:29:00 +0100

    c pulse

--- qse/calc/pulser.py
+++ qse/calc/pulser.py
@@ -208,6 +208,10 @@ def _format_pulse(pulse):
     if pulse is None or isinstance(pulse, pulser.waveforms.Waveform):
         return pulse
     if isinstance(pulse, Signal):
+        if len(pulse.values) == 1:
+            return pulser.waveforms.ConstantWaveform(
+                duration=pulse.duration, value=pulse.values
+            )
         return pulser.waveforms.InterpolatedWaveform(
             duration=pulse.duration, values=pulse.values
         )
