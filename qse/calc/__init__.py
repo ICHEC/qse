9a56b80dae2937b6f45d27e30e36fd572dfc1be0
James Nelson <ja94nelson@gmail.com>
2026-03-25 15:16:43 +0000

    init

--- qse/calc/__init__.py
+++ qse/calc/__init__.py
@@ -5,9 +5,11 @@ __all__ = [
     "ExactSimulator",
     "Pulser",
     "Myqlm",
+    "Qutip",
 ]
 
 from qse.calc.calculator import Calculator
 from qse.calc.exact import ExactSimulator
 from qse.calc.myqlm import Myqlm
 from qse.calc.pulser import Pulser
+from qse.calc.qutip import Qutip
